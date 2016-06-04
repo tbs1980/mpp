c29a399c825a1f02e8431faf042a35a5a0ac04cf
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2016-06-04 22:30:57 +0100

    config and versions headers added

---
+++ include/mpp.hpp
@@ -0,0 +1,4 @@
+#ifndef MPP_MPP_HPP
+#define MPP_MPP_HPP
+
+#endif //MPP_MPP_HPP
---
+++ include/mpp/version.hpp
@@ -0,0 +1,9 @@
+#ifndef MPP_VERSION_HPP
+#define MPP_VERSION_HPP
+
+#define MPP_VERSION_MAJOR 0
+#define MPP_VERSION_MINOR 1
+#define MPP_VERSION_PATCH 0
+
+
+#endif // MPP_VERSION_HPP
