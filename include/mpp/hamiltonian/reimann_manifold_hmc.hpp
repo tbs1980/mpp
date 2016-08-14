333530b4cd54c23cf8572be2ad9902056f92ea46
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2016-08-14 21:15:57 +0100

    rmhmc sv moethod completed

--- include/mpp/hamiltonian/reimann_manifold_hmc.hpp
+++ include/mpp/hamiltonian/reimann_manifold_hmc.hpp
@@ -71,41 +71,55 @@ public:
     static void stomer_verlet(
         std::size_t const num_leap_frog_steps,
         std::size_t const num_fixed_point_steps,
+        real_scalar_t const step_size,
+        log_post_func_t & log_posterior,
+        grad_log_post_func_t & grad_log_posterior,
         mtr_tnsr_log_post_func_t & mtr_tnsr_log_posterior,
         mtr_tnsr_der_log_post_func_t & mtr_tnsr_der_log_posterior,
         real_vector_t & p_new,
-        real_vector_t & theta_new,
+        real_vector_t & x_new
     ) {
-        typedef boost::multi_array<real_scalar_t, p_new.size()> multi_array_t;
-        const double c_e = 1e-4;
-        real_matrix_t G = mtr_tnsr_log_posterior(theta_new);
+        using namespace boost::numeric::ublas;
+        typedef boost::multi_array<real_scalar_t, num_dims> multi_array_t;
+        real_scalar_t const c_e = 1e-4;
+        std::size_t const num_dims = num_dims;
+        real_scalar_t log_post_x0 = log_posterior(x_new);
+        real_matrix_t G = mtr_tnsr_log_posterior(x_new);
+        real_scalar_t det_G = ???
+        real_scalar_t H_0 = log_post_x0 +
         real_matrix_t invG = compute_inverse<real_scalar_t>(G);
-        multi_array_t d_G = mtr_tnsr_der_log_posterior(theta_new);
+        multi_array_t d_G = mtr_tnsr_der_log_posterior(x_new);
         multi_array_t invG_dG_invG(d_G);
-        for(std::size_t ind_i = 0; ind_i < p_new.size(); ++ind_i ){
-            real_matrix_t d_G_i(p_new.size(),p_new.size());
-            for(std::size_t ind_j = 0; ind_j < p_new.size(); ++ind_j){
-                for(std::size_t ind_k =0; ind_k < p_new.size(); ++ind_k){
+        real_vector_t tr_invG_dG(num_dims);
+        for(std::size_t ind_i = 0; ind_i < num_dims; ++ind_i ){
+            real_matrix_t d_G_i(num_dims,num_dims);
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                for(std::size_t ind_k =0; ind_k < num_dims; ++ind_k){
                     d_G_i(ind_j,ind_k) = d_G[ind_i][ind_j][ind_k];
                 }
             }
             real_matrix_t invG_dG_invG_i = prod(invG,d_G_i);
-            for(std::size_t ind_j = 0; ind_j < p_new.size(); ++ind_j){
-                for(std::size_t ind_k =0; ind_k < p_new.size(); ++ind_k){
+            tr_invG_dG(ind_i) = 0.;
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                tr_invG_dG(ind_i) += invG_dG_invG_i(ind_j,ind_j);
+            }
+            invG_dG_invG_i = prod(invG_dG_invG_i,invG);
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                for(std::size_t ind_k =0; ind_k < num_dims; ++ind_k){
                     invG_dG_invG[ind_i][ind_j][ind_k]
                         = invG_dG_invG_i(ind_j,ind_k);
                 }
             }
         }
-        real_vector_t p_0(p_new);
-        real_vector_t old_v(p_new);
 
+        real_vector_t p_0(p_new);
+        real_scalar_t norm_p_0 = norm_2(p_0);
+        real_vector_t pT_invG_dG_invG_p(num_dims);
         for(std::size_t ind_i = 0; ind_i < num_fixed_point_steps; ++i){
-            real_vector_t pT_invG_dG_invG_p(p_new.size());
-            for(std::size_t ind_j = 0; ind_j < p_new.size(); ++ ind_j) {
-                real_matrix_t invG_dG_invG_i(p_new.size(),p_new.size());
-                for(std::size_t ind_k = 0; ind_k < p_new.size(); ++ind_k){
-                    for(std::size_t ind_l =0; ind_l < p_new.size(); ++ind_l){
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ ind_j) {
+                real_matrix_t invG_dG_invG_i(num_dims,num_dims);
+                for(std::size_t ind_k = 0; ind_k < num_dims; ++ind_k){
+                    for(std::size_t ind_l =0; ind_l < num_dims; ++ind_l){
                         invG_dG_invG_i(ind_k,ind_l)
                             = invG_dG_invG[ind_j][ind_k][ind_l];
                     }
@@ -113,7 +127,71 @@ public:
                 pT_invG_dG_invG_p(ind_j)
                     = inner_prod( p_new, prod(invG_dG_invG_i,p_new) );
             }
+            p_new = p_0 - step_size*0.5*(
+                grad_log_posterior(x_new)
+                + 0.5*tr_invG_dG
+                - 0.5*pT_invG_dG_invG_p
+            );
+            real_scalar_t norm_p_new = norm_2(p_new);
+            if( norm_2(p_0/norm_p_0 - p_new/norm_p_new) < c_e ){
+                break;
+            }
         }
+
+        real_vector_t x_0(x_new);
+        real_matrix_t invG_new(invG);
+        real_scalar_t norm_x_0 = norm_2(x_0);
+        for(std::size_t ind_i = 0; ind_i < num_fixed_point_steps; ++i){
+            x_new = x_0 + step_size*0.5*( prod(invG+invG_new,p_new) );
+            G = mtr_tnsr_log_posterior(x_new);
+            invG_new = compute_inverse<real_scalar_t>(G);
+            real_scalar_t norm_x_new = norm_2(x_new);
+            if( norm_2(x_0/norm_x_0 - x_new/norm_x_new) < c_e ){
+                break;
+            }
+        }
+
+        invG = invG_new;
+        d_G = mtr_tnsr_der_log_posterior(x_new);
+        for(std::size_t ind_i = 0; ind_i < num_dims; ++ind_i ){
+            real_matrix_t d_G_i(num_dims,num_dims);
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                for(std::size_t ind_k =0; ind_k < num_dims; ++ind_k){
+                    d_G_i(ind_j,ind_k) = d_G[ind_i][ind_j][ind_k];
+                }
+            }
+            real_matrix_t invG_dG_invG_i = prod(invG,d_G_i);
+            tr_invG_dG(ind_i) = 0.;
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                tr_invG_dG(ind_i) += invG_dG_invG_i(ind_j,ind_j);
+            }
+            invG_dG_invG_i = prod(invG_dG_invG_i,invG);
+            for(std::size_t ind_j = 0; ind_j < num_dims; ++ind_j){
+                for(std::size_t ind_k =0; ind_k < num_dims; ++ind_k){
+                    invG_dG_invG[ind_i][ind_j][ind_k]
+                        = invG_dG_invG_i(ind_j,ind_k);
+                }
+            }
+        }
+
+        for(std::size_t ind_j = 0; ind_j < num_dims; ++ ind_j) {
+            real_matrix_t invG_dG_invG_i(num_dims,num_dims);
+            for(std::size_t ind_k = 0; ind_k < num_dims; ++ind_k){
+                for(std::size_t ind_l =0; ind_l < num_dims; ++ind_l){
+                    invG_dG_invG_i(ind_k,ind_l)
+                        = invG_dG_invG[ind_j][ind_k][ind_l];
+                }
+            }
+            pT_invG_dG_invG_p(ind_j)
+                = inner_prod( p_new, prod(invG_dG_invG_i,p_new) );
+        }
+        p_new = p_new - step_size*0.5*(
+            grad_log_posterior(x_new)
+            + 0.5*tr_invG_dG
+            - 0.5*pT_invG_dG_invG_p
+        );
+
+
     }
 
 private:
