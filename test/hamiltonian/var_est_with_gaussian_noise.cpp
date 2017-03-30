51f6aac0b397997c193adf535837034d5312cea7
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-03-30 18:21:09 +0100

    var estmation init now compiles fine

--- test/hamiltonian/var_est_with_gaussian_noise.cpp
+++ test/hamiltonian/var_est_with_gaussian_noise.cpp
@@ -15,6 +15,7 @@ class variance_estimation {
 public:
     typedef _real_scalar_t real_scalar_t;
     typedef boost::numeric::ublas::vector<real_scalar_t> real_vector_t;
+    typedef boost::numeric::ublas::matrix<real_scalar_t> real_matrix_t;
     typedef boost::numeric::ublas::unbounded_array<real_matrix_t> real_matrix_array_t;
     typedef std::mt19937 rng_t;
     typedef std::normal_distribution<real_scalar_t> normal_distribution_t;
@@ -31,7 +32,6 @@ public:
         BOOST_ASSERT(arg_omega > 0);
         BOOST_ASSERT(arg_zeta > 0);
         m_n = arg_n;
-        m_omega = arg_omega;
         m_zeta = arg_zeta;
 
         // draw {x_i} from a normal distribution with mu=0 and var = arg_omega
@@ -50,7 +50,7 @@ public:
     }
 
     real_scalar_t log_posterior(real_vector_t const & arg_z) const {
-        BOOST_ASSERT(arg_x.size() == m_n + 1); // x and omega
+        BOOST_ASSERT(arg_z.size() == m_n + 1); // x and omega
 
         // arg_z -> x and omega
         real_vector_t x(m_n);
@@ -77,7 +77,7 @@ public:
     }
 
     real_vector_t grad_log_posterior(real_vector_t const & arg_z) const {
-        BOOST_ASSERT(arg_x.size() == m_n + 1); // x and omega
+        BOOST_ASSERT(arg_z.size() == m_n + 1); // x and omega
 
         // arg_z -> x and omega
         real_vector_t x(m_n);
@@ -101,42 +101,70 @@ public:
         d_omega -= 0.5*static_cast<real_scalar_t>(m_n)/omega;
 
         // assign d_x and d_omega to d_z
-        real_vector_t d_z(m_n + 1)
+        real_vector_t d_z(m_n + 1);
+        for(std::size_t i = 0; i < m_n; ++i) {
+            d_z(i) = d_x(i);
+        }
+        d_x(m_n) = d_omega;
+
+        return d_z;
     }
 
     real_matrix_t metric_tensor_log_posterior(
         real_vector_t const & arg_z
     ) const {
         using namespace boost::numeric::ublas;
-        BOOST_ASSERT(arg_x.size() == m_n + 1); // x and omega
+        BOOST_ASSERT(arg_z.size() == m_n + 1); // x and omega
 
         // get omega from z
         real_scalar_t const omega = arg_z(m_n);
+
+        // assign the top right block of the metric tensor
         real_matrix_t G = zero_matrix<real_scalar_t>(m_n+1 ,m_n+1);
         for(std::size_t i = 0; i < m_n; ++i) {
             for(std::size_t j = 0; j< m_n; ++j){
-                G(i,j) = (1./m_zeta + 1./omega)
+                G(i,j) = (1./m_zeta + 1./omega);
             }
         }
-        G(m_n,m_n) = 0.5*static_cast<real_scalar_t>(m_n)/omega/omega
+        // assign the bottom left blcok of the metric tensor
+        G(m_n,m_n) = 0.5*static_cast<real_scalar_t>(m_n)/omega/omega;
+
+        return G;
     }
 
     real_matrix_array_t deriv_metric_tensor_log_posterior(
-        real_vector_t const & arg_x
+        real_vector_t const & arg_z
     ) const {
+        using namespace boost::numeric::ublas;
+        BOOST_ASSERT(arg_z.size() == m_n + 1); // x and omega
 
+        // get omega from z
+        real_scalar_t const omega = arg_z(m_n);
+
+        // assign the derivatives with respect to omega
+        real_matrix_array_t d_G(m_n + 1,zero_matrix<real_scalar_t>(m_n + 1,m_n + 1));
+        for(std::size_t i = 0; i < m_n; ++i) {
+            d_G[m_n](i,i) = -1./omega/omega;
+        }
+        d_G[m_n](m_n,m_n) = -static_cast<real_scalar_t>(m_n)/omega/omega/omega;
+
+        return d_G;
     }
 
 private:
     real_scalar_t m_n;
-    real_scalar_t m_omega;
-    real_scalar_t m_zeta
+    real_scalar_t m_zeta;
     real_vector_t m_y;
 };
 
 template<typename real_scalar_t>
-test_var_est_gaussian_noise(){
-    variance_estimation<real_scalar_t> var_est;
+void test_var_est_gaussian_noise(){
+
+    std::size_t const n = 1000;
+    real_scalar_t const omega(1.);
+    real_scalar_t const zeta(1.);
+    std::size_t const seed = 31415;
+    variance_estimation<real_scalar_t> var_est(n, omega, zeta, seed);
 }
 
 BOOST_AUTO_TEST_CASE(variance_estimation_gaussian_noise) {
